49ba7bacfc17baf4746a2cb5465e8245d442f8dc
Vladyslav <vladkud1324@gmail.com>
2022-06-08 18:36:07 +0300

    initial commit

--- DiplimaDjangoProject/settings.py
+++ DiplimaDjangoProject/settings.py
@@ -25,11 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-NAME = os.environ.get('NAME')
-HOST = os.environ.get('HOST')
-PORT = os.environ.get('PORT')
-USER = os.environ.get('USER')
-PASSWORD = os.environ.get('PASSWORD')
+DB_NAME = os.environ.get('DB_NAME')
+DB_HOST = os.environ.get('DB_HOST')
+DB_PORT = os.environ.get('DB_PORT')
+DB_USER = os.environ.get('DB_USER')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
