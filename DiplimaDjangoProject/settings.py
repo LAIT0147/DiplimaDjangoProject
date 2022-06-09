7a42e4a6e489806b49fe85de3f9b886c6877fd2b
Vladyslav <vladkud1324@gmail.com>
2022-06-09 15:54:39 +0300

    h

--- DiplimaDjangoProject/settings.py
+++ DiplimaDjangoProject/settings.py
@@ -32,7 +32,7 @@ DB_USER = os.environ.get('DB_USER')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['diploma-django-project.herokuapp.com']
 
